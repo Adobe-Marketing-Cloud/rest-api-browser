0197d9fa7d9366ad363c3b26a78f4345ea220923
Julian Sedding <julian.sedding@code-distillery.net>
2015-03-04 11:41:03 +0100

    cleanup build and fix dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -17,19 +17,15 @@
  */
 
 var gulp = require('gulp');
-var rimraf = require('gulp-rimraf');
+var del = require('del');
 var connect = require('gulp-connect');
 var proxy = require('proxy-middleware');
-var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-//var inject = require('gulp-inject');
-
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('clean', function (callback) {
-  return gulp.src('build')
-    .pipe(rimraf());
+  del('./build', callback);
 });
 
 gulp.task('copy-html', function () {
@@ -52,28 +48,12 @@ gulp.task('process-js', browserifyTask({
     require: []
 }));
 
-function browserifyTask(config) {
-  var browserify = require('browserify');
-  var source = require('vinyl-source-stream');
-
-  function bundle() {
-    var bundler = browserify({ debug: true });
-    bundler.add(config.src);
-    bundler.transform('browserify-ngannotate');
-    bundler.bundle()
-      .pipe(source(config.filename))
-      .pipe(gulp.dest(config.dest));
-  }
-  return bundle;
-}
-
 gulp.task('copy-bootstrap', function () {
   return gulp.src('./node_modules/bootstrap/dist/**/*', {
     base: './node_modules/bootstrap/dist/'
   })
     .pipe(gulp.dest('build/vendor/'));
 });
-
 
 gulp.task('webserver', function () {
   connect.server({
@@ -117,80 +97,14 @@ gulp.task('build', ['copy', 'process-js']);
 gulp.task('serve', ['build', 'watch', 'webserver']);
 gulp.task('default', ['build']);
 
-//gulp.task('process-js', function() {
-
-//
-//  return gulp.src('app/scripts/app.js')
-//    .pipe(browserified)
-//    .pipe(gulp.dest('build/js/'))
-//    .pipe(connect.reload());
-//});
-
-function jsTask(config) {
-  var browserify = require('browserify');
-  var transform = require('vinyl-transform');
-  var buffer = require('vinyl-buffer');
-  var bundler = browserify();
-  var browserified = transform(function(filename) {
-    console.log('stream ', arguments);
-    bundler.add(filename);
-    bundler.external(['angular', 'angular-ui-router', 'angular-bootstrap-npm', 'siren']);
-    return bundler.bundle();
-  });
-
-
-  function concatenate() {
-    return gulp.src(config.src)
-      .pipe(sourcemaps.init())
-      .pipe(concat(config.filename))
-      .pipe(ngAnnotate())
-      .pipe(browserified)
-      //.pipe(uglify())
-      //.pipe(rename({extname: ".min.js"})
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(config.dest))
-      .pipe(connect.reload());
-  }
-  return concatenate;
-}
-
-function browserifyTask2(config) {
-  var browserify = require('browserify');
-  var through = require('through2');
-
-  var browserifier = through.obj(function (file, enc, next) {
-    var options = {
-      debug: true
-
-    };
-    browserify(file.path, options)
-
-
-    //browserify(file.path, {debug: true})
-      .require(config.require)
-      .bundle(function (err, res) {
-        if (err) {
-          return next(err);
-        }
-        file.contents = res;
-        next(null, file);
-      })
-  });
-
+function browserifyTask(config) {
   function bundle() {
-    return gulp.src(config.src)
-      //.pipe(ngAnnotate())
-      .pipe(browserifier)
-      //.pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-      //.pipe(uglify())
-      //.pipe(rename({extname: ".min.js"})
-      //.pipe(sourcemaps.write('./')) // writes .map file
-      .pipe(gulp.dest(config.dest))
-      //.pipe(connect.reload());
+    var bundler = browserify({ debug: true });
+    bundler.add(config.src);
+    bundler.transform('browserify-ngannotate');
+    bundler.bundle()
+      .pipe(source(config.filename))
+      .pipe(gulp.dest(config.dest));
   }
-
   return bundle;
 }
-
-
-
